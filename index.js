eec983f9153cee69863261c96d56989f248f71f6
Jason Yu <ycm.jason@gmail.com>
2017-02-14 16:13:15 +0000

    first version

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
+var grab_text = require('grab-text');
 var app = express();
 
 var PORT = process.env.PORT || 8080;
@@ -7,8 +8,12 @@ var PORT = process.env.PORT || 8080;
 app.use(morgan('dev'));
 app.use(express.static('static'));
 
-app.get('/api', (req, res) => {
-  res.send('hello api');
+app.get('/api/grabtext', (req, res) => {
+  var url = req.query.url;
+  grab_text(url).then(text => res.json({
+    url: url,
+    text: text
+  }));
 });
 
 app.listen(PORT, function(){
