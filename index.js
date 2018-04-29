64431fcb4d2cb05965f5b1c59f19fc610f7a1552
Jason Yu <me@ycmjason.com>
2018-04-29 21:17:06 +0100

    1.2.0

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ const PORT = process.env.PORT || 8080;
 app.use(morgan('dev'));
 app.use(express.static('static'));
 
+const EMPTY_LYRICS = {
+  title: '',
+  artist: '',
+  lyrics: '',
+};
+
 app.get('/api/grab', (req, res) => {
   const url = req.query.url;
+  if (!url) return res.json(EMPTY_LYRICS);
+
   grabLyrics(url).then(({ title, artist, lyrics }) => res.json({
     title,
     artist,
